f8f28c7c2da79636b1f44795cfafbf7418b247ee
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-15 23:36:53 +0100

    Refactor Create function and add Loading

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -6,36 +6,50 @@ const Create = () => {
   // https://reactrouter.com/6.29.0/hooks/use-navigate
   const navigate = useNavigate()
 
+  const [isLoading, setIsLoading] = useState(false)
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [rating, setRating] = useState('')
   const [formError, setFormError] = useState(null)
 
   async function handleSubmit(e) {
-    e.preventDefault()
+    try {
+      e.preventDefault()
 
-    if (!title || !method || !rating) {
-      setFormError('Fill in all fields')
-      return
-    }
+      setIsLoading(true)
+      if (!title || !method || !rating) {
+        setFormError('Fill in all fields')
+        return
+      }
 
-    const { data, error } = await supabase
-      .from('smoothies')
-      .insert([
-        {
-          title: title,
-          method: method,
-          rating: rating,
-        },
-      ])
-      .select()
+      const { data, error } = await supabase
+        .from('smoothies')
+        .insert([
+          {
+            title: title,
+            method: method,
+            rating: rating,
+          },
+        ])
+        .select()
 
-    if (data) {
+      if (error) {
+        console.error(error)
+        throw new Error(error)
+      }
+
+      if (!data) {
+        return
+      }
+
       console.log(data)
+
+      const { id } = data || {}
+
       navigate('/')
-    }
-    if (error) {
-      console.log(error)
+    } catch (error) {
+    } finally {
+      setIsLoading(true)
     }
   }
 
@@ -44,26 +58,26 @@ const Create = () => {
       <h2>Create</h2>
 
       <form onSubmit={handleSubmit}>
+        {formError && <p>{formError}</p>}
         <label htmlFor="title">Title</label>
         <input
           type="text"
           id="title"
           name="title"
           value={title}
-          placeholder="Title"
+          placeholder=""
           onChange={e => {
             setTitle(e.target.value)
           }}
+          disabled={isLoading}
         />
         <label htmlFor="method">Method</label>
-        <textarea name="method" id="method" value={method} onChange={e => setMethod(e.target.value)}></textarea>
+        <textarea name="method" id="method" value={method} onChange={e => setMethod(e.target.value)} disabled={isLoading}></textarea>
 
         <label htmlFor="rating">Rating (0 to 10)</label>
-        <input type="number" id="rating" name="rating" min="0" max="10" step="1" value={rating} onChange={e => setRating(e.target.value)} />
+        <input type="number" id="rating" name="rating" min="0" max="10" step="1" value={rating} onChange={e => setRating(e.target.value)} disabled={isLoading} />
 
-        <button>Create</button>
-
-        {formError && <p>{formError}</p>}
+        <button disabled={isLoading}>Create</button>
       </form>
     </div>
   )
