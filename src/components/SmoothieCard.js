e83c267dc8c8d759a030231a92f10b1346046b9c
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-15 22:43:03 +0100

    Add edit button to SmoothieCard

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom'
+
 export default function SmoothieCard(props) {
   const {
-    smoothie: { title, method, rating },
+    smoothie: { id, title, method, rating },
   } = props
   return (
     <div className="smoothie-card">
       <h3>{title}</h3>
       <p>{method}</p>
       <div className="rating">{rating}</div>
+      <Link to={`/${id}`}>
+        <i className="material-icons">edit</i>
+      </Link>
     </div>
   )
 }
