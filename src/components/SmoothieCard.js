f4c3bccab3f160e5f7e6ce42050388086527336f
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-16 01:23:38 +0100

    Update SmoothieCard to delete a smoothie

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom'
+import supabase from '../config/supabaseConfig'
 
 export default function SmoothieCard(props) {
   const {
     smoothie: { id, title, method, rating },
   } = props
+
+  async function handleDelete() {
+    try {
+      const { data, error } = await supabase.from('smoothies').delete().eq('id', id).select()
+
+      if (error) {
+        console.error(error)
+        throw new Error(error)
+      }
+
+      if (!data) {
+        return
+      }
+
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div className="smoothie-card">
       <h3>{title}</h3>
@@ -12,6 +32,9 @@ export default function SmoothieCard(props) {
       <Link to={`/${id}`}>
         <i className="material-icons">edit</i>
       </Link>
+      <i className="material-icons" onClick={handleDelete}>
+        delete
+      </i>
     </div>
   )
 }
