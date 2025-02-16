5863464008eb7331b9988ee8367035dfce81a216
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-16 01:13:52 +0100

    Add docblock comment

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,13 @@
+/**
+ * Pauses execution for a specified amount of time.
+ *
+ * @param {number} [ms=1000] - The number of milliseconds to wait before resolving.
+ * @returns {Promise<void>} A promise that resolves after the specified duration.
+ *
+ * @example
+ * // Pause execution for 2 seconds
+ * await sleep(2000);
+ */
 export async function sleep(ms = 1000) {
   await new Promise(r => setTimeout(r, ms))
 }
