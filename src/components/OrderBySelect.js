b21b8dd29c50cb0ef56251aa39d8b53e8515bc18
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-16 17:06:30 +0100

    Set value using value attribute

--- src/components/OrderBySelect.js
+++ src/components/OrderBySelect.js
@@ -1,5 +1,5 @@
 export default function OrderBySelect(props) {
-  const { setOrderBy } = props || {}
+  const { orderBy, setOrderBy } = props || {}
 
   function handleSelectChange(e) {
     setOrderBy(e.target.value)
@@ -8,11 +8,9 @@ export default function OrderBySelect(props) {
   return (
     <>
       <p>Order by:</p>
-      <select name="orderBy" id="orderBy" className="form-control" onChange={handleSelectChange}>
+      <select name="orderBy" id="orderBy" className="form-control" onChange={handleSelectChange} value={orderBy}>
         <option disabled>Order by</option>
-        <option value="created_at" selected>
-          Created
-        </option>
+        <option value="created_at">Created</option>
         <option value="title">Title</option>
         <option value="rating">Rating</option>
       </select>
