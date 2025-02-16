e9f232e1a5a52ce85cc672685920e1fa8a5cfdb0
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-16 14:19:37 +0100

    Add ordering smoothies functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at') // created_at is the column name
 
   function updateSmoothiesAfterDeletion(id) {
     setSmoothies(prev => {
@@ -17,7 +18,7 @@ const Home = () => {
     async function fetchSmoothies() {
       // .select() means select all (like SELECT *)
       // ascending false means bigger id comes before the smaller id (newer row first)
-      const { data, error } = await supabase.from('smoothies').select().order('id', { ascending: false })
+      const { data, error } = await supabase.from('smoothies').select().order(orderBy, { ascending: false })
 
       if (error) {
         setFetchError('Could not fetch smoothies')
@@ -32,7 +33,7 @@ const Home = () => {
     }
 
     fetchSmoothies()
-  }, [])
+  }, [orderBy])
 
   return (
     <div className="page home">
@@ -42,6 +43,13 @@ const Home = () => {
 
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            {orderBy}
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
               <SmoothieCard key={smoothie?.id} smoothie={smoothie} onDelete={updateSmoothiesAfterDeletion} />
