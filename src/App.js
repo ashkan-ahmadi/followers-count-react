cd599aeaf72f0ce838a50939649083da50cd3995
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-02-16 14:29:39 +0100

    Update App to include BS classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 // pages
-import Home from "./pages/Home"
-import Create from "./pages/Create"
-import Update from "./pages/Update"
-
+import Home from './pages/Home'
+import Create from './pages/Create'
+import Update from './pages/Update'
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <h1>Supa Smoothies</h1>
-        <Link to="/">Home</Link>
-        <Link to="/create">Create New Smoothie</Link>
-      </nav>
+      <header className="container-fluid py-4 bg-light ">
+        <div className="container">
+          <nav className="navbar text-center ">
+            <a className="navbar-brand">Supabase Smoothies</a>
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/create">
+                <a className="btn btn-primary">Create New Smoothie</a>
+              </Link>
+            </div>
+          </nav>
+        </div>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/:id" element={<Update />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
