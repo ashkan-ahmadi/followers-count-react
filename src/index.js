08a600d1a6f059c4898ea3eb6df3ab16addfb599
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-03-06 18:25:40 +0100

    Remove ToastContainer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 // import './index.css'
 import './bootstrap.min.css'
 import App from './App'
-import { ToastContainer } from 'react-toastify'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
   </React.StrictMode>
 )
