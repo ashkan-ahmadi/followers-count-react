bb5b7084494bac0e51fe9385c9e5c723c851930c
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-03-07 22:37:50 +0100

    Add new util functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -66,3 +66,28 @@ export function showWarningToast(text = 'Warning', options = {}) {
 export function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
 }
+
+/**
+ * Format numbers using the native Intl.NumberFormat object
+ *
+ * Note: Intl.NumberFormat() can be called with or without new. Both create a new Intl.NumberFormat instance.
+ * However, there's a special behavior when it's called without new and the this value is another Intl.NumberFormat instance;
+ * see Return value: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat#return_value
+ *
+ * @param {string} locale - A string with a BCP 47 language tag or an Intl.Locale instance, or an array of such locale identifiers. The runtime's default locale is used when undefined is passed or when none of the specified locale identifiers is supported.
+ * @param {object} [options={}] - An object. For ease of reading, the property list is broken into sections based on their purposes, including locale options, style options, digit options, and other options.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+ *
+ * @return {object} - A new Intl.NumberFormat object.
+ */
+export function formatNumber(locale = undefined, options = {}) {
+  return new Intl.NumberFormat(locale, options)
+}
+
+export const SECOND_IN_MILLISECONDS = 1000
+
+export const MINUTE_IN_MILLISECONDS = 60 * SECOND_IN_MILLISECONDS
+export const HOUR_IN_MILLISECONDS = 60 * MINUTE_IN_MILLISECONDS
+export const DAY_IN_MILLISECONDS = 24 * HOUR_IN_MILLISECONDS
+export const WEEK_IN_MILLISECONDS = 7 * DAY_IN_MILLISECONDS
