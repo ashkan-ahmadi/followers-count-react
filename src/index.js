a1dd068be26b9b42ff3c899e5c4ce6496a1c9f3b
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-03-07 22:43:46 +0100

    Delete App and load Home directly

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
-import App from './App'
+import Home from './pages/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 )
