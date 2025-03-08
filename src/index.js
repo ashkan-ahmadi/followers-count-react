ab1c52bbc2668b739e0611c80aeb543cde2c2682
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-03-09 00:14:54 +0100

    Disable StrictMode

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import Home from './pages/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <>
+    {/* <React.StrictMode> */}
     <Home />
-  </React.StrictMode>
+    {/* </React.StrictMode> */}
+  </>
 )
