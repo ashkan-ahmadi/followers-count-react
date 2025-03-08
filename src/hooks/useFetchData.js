070f1c4870901184642f27b33b6071803f5d8e1b
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-03-09 00:15:09 +0100

    Fix issue with custom hook and setInterval

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { getRandomNumber, sleep } from '../utils/utils'
 
-export default function useFetchData() {
+export default function useFetchData(autoFetch = 0) {
   const [currentValue, setCurrentValue] = useState(null)
   const [previousValue, setPreviousValue] = useState(null)
   const [loading, setLoading] = useState(false)
@@ -12,18 +12,25 @@ export default function useFetchData() {
       setLoading(true)
 
       // simulating slow API response to see the loading spinner
-      await sleep(500)
-
-      // FIXME: not sure why this part doesn't run
-      // number always returns null even on the re-renders
-      if (currentValue) {
-        setPreviousValue(currentValue)
-      }
+      await sleep(1000)
 
       // Fetch a random number
       // NOT USING FETCH TO AVOID CORS FOR NOW
-      const randomNumber = getRandomNumber(2322, 988478)
-      setCurrentValue(randomNumber)
+      const randomNumber = getRandomNumber(1, 100)
+
+      // we do it this way so the interval works
+      // Doing this below does NOT work for the interval
+      // setPreviousValue(currentValue)
+      // setCurrentValue(randomNumber)
+      setCurrentValue(prevCurrent => {
+        if (prevCurrent) {
+          setPreviousValue(prevCurrent) // Update previousValue inside functional update
+        }
+        return randomNumber
+      })
+
+      // setPreviousValue(currentValue)
+      // setCurrentValue(randomNumber)
     } catch (error) {
       console.log(error)
       setError(error)
@@ -36,11 +43,18 @@ export default function useFetchData() {
   // subsequent fetches are handled by the useEffect below through setInterval
   useEffect(() => {
     fetchValue()
-
-    // fetch number on the very first use of the hook
-    const interval = setInterval(fetchValue, 2000) // Refresh at an interval
-    return () => clearInterval(interval)
   }, [])
 
-  return { currentValue, previousValue, loading, error }
+  useEffect(() => {
+    // fetch number on the very first use of the hook
+
+    if (autoFetch) {
+      const interval = setInterval(() => {
+        fetchValue()
+      }, autoFetch) // Refresh at an interval
+      return () => clearInterval(interval)
+    }
+  }, [])
+
+  return { currentValue, previousValue, loading, error, fetchValue }
 }
