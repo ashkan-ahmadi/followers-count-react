bf2e8cf2d1d534723ae0c84e8033a0b5d9e2094c
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-06-24 15:42:04 +0200

    Take elements out of Home and into separate components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import MetricCard from '../components/MetricCard'
-import { AUTO_REFRESH } from '../constants'
+import Cards from '../components/Cards'
+import DescriptionText from '../components/DescriptionText'
+import Header from '../components/Header'
+import InfoAlertBox from '../components/InfoAlertBox'
 
 const Home = () => {
   // ids are fake values
@@ -18,34 +20,13 @@ const Home = () => {
     <>
       <main className="bg-body-tertiary vh-100 bg-light">
         <section className="container py-4">
-          <h1 className="mb-4">Followers</h1>
+          <Header />
 
-          {AUTO_REFRESH ? (
-            <p className="text-muted mb-4">
-              The metrics auto-update every <b>{AUTO_REFRESH / 1000} seconds</b>.
-            </p>
-          ) : null}
+          <DescriptionText />
 
-          <div class="alert alert-primary d-flex gap-2 align-items-center mb-4">
-            <div>
-              <i class="bi bi-info-circle-fill"></i>
-            </div>
-            <p className="mb-0">
-              You can use the <code>Chrome DevTools &gt; Sensors &gt; Location</code> to override the locale and modify the number formatting.
-            </p>
-          </div>
+          <InfoAlertBox />
 
-          {cards?.length ? (
-            <div className="row g-4">
-              {cards.map(card => (
-                <div className="col-12 col-sm-6 col-md-4 col-xl-3 " key={card.id}>
-                  <MetricCard heading={card?.heading} icon={card?.icon} />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div className="alert alert-warning">No cards found</div>
-          )}
+          <Cards cards={cards} />
         </section>
       </main>
     </>
