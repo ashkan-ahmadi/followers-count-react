315072276f95a6c2aaed125a5e405b0b23ebaad2
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2025-06-25 13:38:22 +0200

    Remove toast functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,3 @@
-import { toast } from 'react-toastify'
-
 /**
  * Pauses execution for a specified amount of time.
  *
@@ -12,55 +10,6 @@ import { toast } from 'react-toastify'
  */
 export async function sleep(ms = 1000) {
   await new Promise(r => setTimeout(r, ms))
-}
-
-/**
- * Display a generic but customizable toast
- *
- * @author Ashkan Ahmadi
- * @param {string} [text=OK] - The text to display
- * @param {object} [options={}] - An object with all the options to customize the behavior and look
- * @example showToast('Success')
- * @example showToast('Success', { position: 'top-left' })
- * @link https://fkhadra.github.io/react-toastify/category/guides
- *
- */
-export function showToast(text = 'OK', options = {}) {
-  const defaultConfigs = {
-    position: 'top-right',
-  }
-
-  const opts = { ...defaultConfigs, ...options }
-
-  toast(text, opts)
-}
-
-export function showErrorToast(text = 'Error', options = {}) {
-  showToast(text, {
-    type: 'error',
-    ...options,
-  })
-}
-
-export function showSuccessToast(text = 'Success', options = {}) {
-  showToast(text, {
-    type: 'success',
-    ...options,
-  })
-}
-
-export function showInfoToast(text = 'Info', options = {}) {
-  showToast(text, {
-    type: 'info',
-    ...options,
-  })
-}
-
-export function showWarningToast(text = 'Warning', options = {}) {
-  showToast(text, {
-    type: 'warning',
-    ...options,
-  })
 }
 
 export function getRandomNumber(min, max) {
